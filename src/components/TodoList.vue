4dbb7e937cb3e4ef93960744421650b3402c6c63
Mike Borozdin <mborozdin@ethoslending.com>
2018-12-25 12:10:39 -0500

    renamed HelloWorld to TodoList

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -22,7 +22,7 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "TodoList",
   props: {
     msg: String
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="home"><HelloWorld msg="Welcome to Your Vue.js App" /></div>
+  <div class="home"><TodoList msg="Welcome to Your Vue.js App" /></div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TodoList from "@/components/TodoList.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    TodoList
   }
 };
 </script>
