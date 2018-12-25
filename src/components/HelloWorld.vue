5c9cc6e66196cf324c5c554e71f0f20c357415ac
Mike Borozdin <mborozdin@ethoslending.com>
2018-12-25 02:18:54 -0500

    working vue.js loading

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,88 +1,22 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
+    <section class="main" v-show="todos.length" v-cloak>
+      <ul class="todo-list">
+        <li
+          v-for="todo in todos"
+          class="todo"
+          :key="todo.id"
+          :class="{ completed: todo.completed }"
         >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+          <div class="view">
+            <input class="toggle" type="checkbox" v-model="todo.completed" />
+            <label>{{ todo.title }}</label>
+            <button class="destroy" @click="removeTodo(todo)"></button>
+          </div>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -91,6 +25,30 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  created() {
+    this.$store.dispatch("loadTodos");
+  },
+  computed: {
+    newTodo() {
+      return this.$store.getters.newTodo;
+    },
+    todos() {
+      return this.$store.getters.todos;
+    }
+  },
+  methods: {
+    setNewTodo(e) {
+      this.$store.dispatch("setNewTodo", e.target.value);
+    },
+    addTodo(e) {
+      e.target.value = "";
+      this.$store.dispatch("addTodo");
+      this.$store.dispatch("clearNewTodo");
+    },
+    removeTodo(todo) {
+      this.$store.dispatch("removeTodo", todo);
+    }
   }
 };
 </script>
@@ -99,14 +57,6 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 a {
   color: #42b983;
--- src/store.js
+++ src/store.js
@@ -1,10 +1,83 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
+function randomId() {
+  return parseInt(Math.random() * 10000000);
+}
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    loading: true,
+    todos: [],
+    newTodo: ""
+  },
+  getters: {
+    newTodo: state => state.newTodo,
+    todos: state => state.todos
+  },
+  mutations: {
+    SET_LOADING(state, flag) {
+      state.loading = flag;
+    },
+    SET_TODOS(state, todos) {
+      state.todos = todos;
+    },
+    SET_NEW_TODO(state, todo) {
+      state.newTodo = todo;
+    },
+    REMOVE_TODO(state, todo) {
+      var todos = state.todos;
+      todos.splice(todos.indexOf(todo), 1);
+    },
+    ADD_TODO(state, todoObject) {
+      state.todos.push(todoObject);
+    },
+    CLEAR_NEW_TODO(state) {
+      state.newTodo = "";
+    }
+  },
+  actions: {
+    loadTodos({ commit }) {
+      console.log("loadTodos action");
+      commit("SET_LOADING", true);
+      axios
+        .get("http://localhost:3000/todos")
+        .then(r => r.data)
+        .then(todos => {
+          commit("SET_TODOS", todos);
+          commit("SET_LOADING", false);
+        });
+    },
+    setNewTodo({ commit }, todo) {
+      commit("SET_NEW_TODO", todo);
+    },
+    addTodo({ commit, state }) {
+      console.log("addTodo action");
+      if (!state.newTodo) {
+        // to do add empty todos
+        return;
+      }
+      const todo = {
+        title: state.newTodo,
+        completed: false,
+        id: randomId()
+      };
+      axios.post("/todos", todo).then(() => {
+        commit("ADD_TODO", todo);
+      });
+    },
+    removeTodo({ commit }, todo) {
+      axios.delete(`/todos/${todo.id}`).then(() => {
+        console.log("removed todo", todo.id, "from the server");
+        commit("REMOVE_TODO", todo);
+      });
+    },
+    clearNewTodo() {
+      console.log("clearNewTodo action");
+      commit("CLEAR_NEW_TODO");
+    }
+  }
 });
